24039767687c4b43888baa8698d8f9d2596bd9c3
barro97 <rosenthb@post.bgu.ac.il>
2025-03-31 15:24:21 +0300

    added dataset schema

---
+++ backend/schemas/dataset.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+
+class DatasetBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+    download_count: Optional[int] = 0
+    uploader_id: int
+
+class DatasetCreate(DatasetBase):
+    pass
+
+class DatasetUpdate(DatasetBase):
+    pass
+
+class Dataset(DatasetBase):
+    id: int
+    created_at: datetime
+    updated_at: Optional[datetime] = None
+
+    class Config:
+        orm_mode = True
+
+
+

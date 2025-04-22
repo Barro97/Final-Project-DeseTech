14ac603e8739de2c38cffe0099451bc9ea65f6e2
barro97 <rosenthb@post.bgu.ac.il>
2025-04-22 20:01:16 +0300

    created a login/signup page

---
+++ frontend/src/app/auth/page.tsx
@@ -0,0 +1,9 @@
+import AuthForms from "@/app/features/auth/components/auth-forms";
+
+export default function AuthPage() {
+  return (
+    <div className="theme-auth">
+      <AuthForms />
+    </div>
+  );
+}
